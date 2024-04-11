f9e2afc5c098dd89af5ced00390330f4879061e6
Muri68 <muriisyaku68@gmail.com>
2024-04-11 10:29:51 +0100

    Edit account admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,8 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'username', 'user_type', 'is_active')
+    list_display = ('email', 'full_name', 'username', 'user_type', 'is_active')
+    list_display_links = ('email', 'username', 'full_name')
     ordering = ('-date_joined',)
     filter_horizontal = ()
     list_filter = ()
